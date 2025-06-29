9434ff3a38426f9f77553b97076448e7af540893
Mani9792 <106470501+Mani9792@users.noreply.github.com>
2025-06-29 12:30:39 +0530

    Update karate-config.js

--- src/test/java/karate-config.js
+++ src/test/java/karate-config.js
@@ -2,17 +2,19 @@ function fn() {
   var env = karate.env; // get system property 'karate.env'
   karate.log('karate.env system property was:', env);
   if (!env) {
-    env = 'qa';
+    env = 'dev';
   }
   var config = {
     env: env,
-    myVarName: 'someValue'
+    myVarName: 'someValue',
+    baseUrl: ''
   }
   if (env == 'dev') {
-    // customize
-    // e.g. config.foo = 'bar';
-  } else if (env == 'e2e') {
-    // customize
+    config.baseUrl = 'https://dev.reqres.in/api/users';
+  } else if (env == 'qa') {
+    config.baseUrl = 'https://qa.reqres.in/api/users';
+  } else if (env == 'prod') {
+    config.baseUrl = 'https://prod.reqres.in/api/users';
   }
   return config;
 }
